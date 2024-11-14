3e7191ba7a091e14bcbd88545f871ac4c897e9ea
isaque-dantas <vicisaque413@gmail.com>
2024-11-14 15:00:19 -0300

    feat: extend token lifetime for development

--- matemates_server/settings.py
+++ matemates_server/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/3.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
-
+from datetime import timedelta
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -79,6 +79,10 @@ REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework_simplejwt.authentication.JWTAuthentication',
     )
+}
+
+SIMPLE_JWT = {
+    "ACCESS_TOKEN_LIFETIME": timedelta(hours=6),
 }
 
 AUTH_USER_MODEL = 'api.User'
